7e6c61833c40255858d895c9b766600773744092
emre g. <emreodev1998@gmail.com>
2025-12-01 05:22:22 +0300

    şşs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import glsl from 'vite-plugin-glsl'
+import { defineConfig } from 'vite'
 
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
-export default {
+
+export default defineConfig({
     root: 'src/',
     publicDir: '../static/',
     base: '/black-hole/',
@@ -21,4 +23,4 @@ export default {
     [
         glsl()
     ]
-}
+})
